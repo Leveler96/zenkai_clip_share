342dcb30295dce98d52473ff2f9e47943744affb
Leveler96 <196593883+Leveler96@users.noreply.github.com>
2025-07-30 14:28:30 -0400

    Implement user login, logout, and dashboard functionality

---
+++ users/urls.py
@@ -0,0 +1,8 @@
+# users/url.py
+from django.urls import path
+from . import views
+
+# this is where app-specific URLs go
+urlpatterns = [
+    path('dashboard/', views.dashboard, name='dashboard'),
+]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+
+# Create views here.
+@login_required  # decorator that ensures that logged in users can access this view
+def dashboard(request):
+    # user object is available in the template automatically if users can access this view
+    return render(request, 'users/dashboard.html')
